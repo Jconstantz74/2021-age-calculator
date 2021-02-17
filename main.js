b7a0c042332f160537464709ebb8f1ed122c4547
Jconstantz74 <70533059+Jconstantz74@users.noreply.github.com>
2021-02-16 20:37:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function age_calc(name, relationship, age) {
-	return `My ${relationship}, ${name}, is ${age} years old (${age*12} months)`
+	return `My ${relationship}, ${name}, is ${age} years old (${age*12} months)`;
 }
+
+console.log(age_calc("Barb", "mom", 62));
+console.log(age_calc("Jeff", "dad", 60));
+console.log(age_calc("Jeff", "brother", 24));
+	    
